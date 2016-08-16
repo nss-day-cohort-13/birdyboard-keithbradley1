2c25254557151e940fd0a6fe9ecc5c537c3085f9
Keith <keithbradley1@msn.com>
2016-08-16 09:07:02 -0500

    edit menu contd

--- menu.py
+++ menu.py
@@ -38,7 +38,7 @@ class Menu:
         name = prompt('Enter user Name')
         screen_name = prompt('Enter screen_name')
 
-        self.bang.create_new_user(name, screen_name)
+        self.bird.create_new_user(name, screen_name)
 
         print('Your new user has been created')
         pause()
@@ -47,82 +47,39 @@ class Menu:
         self.clear_menu()
 
         user_menu = {
-            '{}. {}'.format(c.id, c.name):c for c in self.bang.users.values()}
+            '{}. {}'.format(c.id, c.name):c for c in self.bird.users.values()}
         chosen_user = show_menu('User List:', user_menu, '')
-        self.bang.select_active_user(chosen_user.id)
+        self.bird.select_active_user(chosen_user.id)
 
         print('You are using Birdyboard as ' + chosen_user.name)
         pause()
 
-    def prompt_create_payment(self):
+    def prompt_create_chirp(self):
         self.clear_menu()
 
-        if self.bang.active_user_id == 0:
+        if self.bird.active_user_id == 0:
             pause('You must choose a user account first')
             return
 
         payment_type = prompt('Enter Payment Type (e.g. AmEx, Visa, Checking)')
         account_number = prompt('Enter Account Number')
-        self.bang.create_new_payment(payment_type, account_number, self.bang.active_user_id)
+        self.bird.create_new_payment(payment_type, account_number, self.bird.active_user_id)
 
         print('New Payment Option created as {} with account number {}'
             .format(payment_type, account_number))
         pause()
 
-    def prompt_add_product(self):
-        self.clear_menu()
 
-        if self.bang.active_user_id == 0:
-            pause('Please choose a user first.')
+    def prompt_create_reply(self):
+        self.clear_menu()
+        if self.bird.active_order_id == 0:
+            pause('You can write a reply')
             return
 
-        while True:
-            self.clear_menu()
-            print('Add Products')
-            product_menu = {
-                '{}. {}'.format(p.id, p.name):p for p in self.bang.products.values()}
-            product_menu['7. Back to main menu'] = None
-            chosen_product = show_menu('Products:', product_menu, '')
 
-            if chosen_product is None: break
-
-            self.bang.create_new_order(self.bang.active_user_id)
-            self.bang.add_product_to_order(self.bang.active_order_id, chosen_product.id)
-
-            order_total = sum([self.bang.products[item.product_id].price
-                    for item in self.bang.order_line_items.values()
-                    if item.order_id == self.bang.active_order_id])
-            print('You have added ' + chosen_product.name + ' to your shopping cart')
-            print('Your current total is ${:.2f}.'.format(order_total))
-            pause('Press enter to continue adding to your cart')
-
-    def prompt_complete_order(self):
+    def print_all_chirps(self):
         self.clear_menu()
-        if self.bang.active_order_id == 0:
-            pause('You must have an active order before you can checkout')
-            return
-
-        order_total = sum([self.bang.products[item.product_id].price
-                for item in self.bang.order_line_items.values()
-                if item.order_id == self.bang.active_order_id])
-        should_continue = prompt('Your order total is ${:.2f}. Pay now? [\033[32mY\033[37m/n]'
-                                    .format(order_total))
-
-        if should_continue and should_continue.lower()[0] == 'n': return
-
-        payment_menu = {
-            '{}. {}'.format(p.id, p.payment_type):p for p in self.bang.payment_options.values()}
-        chosen_payment = show_menu('Choose Your Payment Method', payment_menu, '')
-        self.bang.pay_order(chosen_payment.id)
-
-        print('Your order is complete! You paid ${:.2f} with your {}.'.format(
-            order_total,
-            chosen_payment.payment_type))
-        pause()
-
-    def print_popular_products(self):
-        self.clear_menu()
-        popular = self.bang.get_popular_products()
+        popular = self.bird.get_all_chirps()
         products = popular['products']
         totals = popular['totals']
 
